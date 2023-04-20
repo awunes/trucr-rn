832ba30fecb9550e987a77ebb2883b62bf83f0d9
TonySwearingen <unlimited1974@aol.com>
2023-04-20 19:30:33 -0400

    Added Owner and Copyright info

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ScrollView, Text, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
+import { View, ScrollView, Text, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -14,66 +15,78 @@ const HomeScreen = ({ navigation }) => {
           style={styles.button}
           onPress={() => navigation.navigate('AL_GA')}
         >
-          <Text>AL-GA</Text>
+          <Text style={styles.buttonText}>AL-GA</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('ID_MA')}
         >
-          <Text>ID-MA</Text>
+          <Text style={styles.buttonText}>ID-MA</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('MI_NM')}
         >
-          <Text>MI-NM</Text>
+          <Text style={styles.buttonText}>MI-NM</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('NY_SD')}
         >
-          <Text>NY-SD</Text>
+          <Text style={styles.buttonText}>NY-SD</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('TN_WY')}
         >
-          <Text>TN-WY</Text>
+          <Text style={styles.buttonText}>TN-WY</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('Canada')}
         >
-          <Text>Canada</Text>
+          <Text style={styles.buttonText}>Canada</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('Mexico')}
         >
-          <Text>Mexico</Text>
+          <Text style={styles.buttonText}>Mexico</Text>
         </TouchableOpacity>
+        <Text style={styles.ownerText}>Built by Tony Swearingen</Text>
+        <Text style={styles.ownerText}>Owned by Swearingen Unlimited LLC</Text>
+        <Text style={styles.ownerText}><AntDesign name="copyright" size={12} color="red" /> - 2023</Text>
       </ImageBackground>
     </ScrollView>
   )
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   button: {
     backgroundColor: 'teal',
     borderRadius: 30,
-    marginLeft: 150,
+    marginLeft: 140,
     marginRight: 150,
     marginTop: 40,
     padding: 10,
   },
-  container: {
-    flex: 1,
+  buttonText: {
+    fontSize: 18,
+    marginLeft: 8,
   },
   image: {
     width: '100%', 
     height: '100%',
     marginBottom: '60%'
-  }
+  },
+  ownerText: {
+    fontSize: 16,
+    marginLeft: 80,
+    marginTop:10,
+  },
 });
 
 export default HomeScreen;
